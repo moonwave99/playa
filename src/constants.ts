8d82dacd28e3a0e3fe94655766202aed8a035194
Diego Caponera <diego.caponera@gmail.com>
2019-12-22 02:09:45 +0100

    Embellish album data display.

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,8 @@
-
 import * as os from 'os'
 
 export const MACOS = (os.platform() === "darwin");
 
 export const HEIGHT = 800;
 export const WIDTH = 1200;
+
+export const VARIOUS_ARTISTS_ID = '_various-artists';
--- src/renderer/components/ResultListItem.tsx
+++ src/renderer/components/ResultListItem.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { ipcRenderer } from 'electron';
 import { Album } from '../../interfaces';
+import { VARIOUS_ARTISTS_ID } from '../../constants';
 
 type ResultListItemProps = {
   result: Album;
@@ -15,8 +16,8 @@ export const ResultListItem: FC<ResultListItemProps> = ({ result }) => {
   return (
     <li className="result-list-item" onClick={onClick}>
       <span className={tagClasses}>{type}</span>
-      <span className="year">{year}</span>
-      <span className="artist">{artist}</span>
+      <span className="year">{year ? year : '-'}</span>
+      <span className="artist">{artist === VARIOUS_ARTISTS_ID ? 'V/A' : artist}</span>
       <span className="title">{title}</span>
     </li>
   );
