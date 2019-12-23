7b17abe8bea4abfebbe70d9d72a10761b2d30e01
Diego Caponera <diego.caponera@gmail.com>
2019-12-23 22:45:26 +0100

    Add jest mock configuration.

--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,9 @@ module.exports = {
   },
   setupFilesAfterEnv: [
     "<rootDir>/test/setup.ts"
-  ]
+  ],
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/styleMock.js",
+    "\\.(gif|ttf|eot|svg)$": "<rootDir>/test/__mocks__/fileMock.js"    
+  }
 }
