446a7ed0f5a56ece6a411260ede8df7ecd4311ab
Diego Caponera <diego.caponera@gmail.com>
2020-01-20 14:22:07 +0100

    Welcome to react-table.

--- src/renderer/components/AllPlaylistsView/AllPlaylistsView.tsx
+++ src/renderer/components/AllPlaylistsView/AllPlaylistsView.tsx
@@ -1,5 +1,6 @@
-import React, { ReactElement, SyntheticEvent, FC } from 'react';
+import React, { ReactElement, SyntheticEvent, FC, useMemo } from 'react';
 import { Link, generatePath } from 'react-router-dom';
+import { useTable, Cell } from 'react-table';
 import { Playlist } from '../../store/modules/playlist';
 import { formatDate } from '../../utils/datetimeUtils';
 import { PLAYLIST_SHOW } from '../../routes';
@@ -14,43 +15,90 @@ export const AllPlaylistsView: FC<AllPlaylistsViewProps> = ({
   playlists = [],
   onPlaylistDelete
 }) => {
-  function renderTableRow(playlist: Playlist): ReactElement {
-    const { _id, title, created, accessed } = playlist;
 
+  const columns = useMemo(() => [{
+     Header: 'Title',
+     accessor: 'title'
+   },
+   {
+     Header: 'Created on',
+     accessor: 'created'
+   },
+   {
+     Header: 'Last accessed',
+     accessor: 'accessed'
+   },
+   {
+     Header: '',
+     id: 'delete',
+     accessor: '_id'
+   }
+ ],[]);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headers,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data: playlists,
+  });
+  
+  function renderCell(cell: Cell): ReactElement {
+    const playlist = cell.row.original as Playlist;
+    const { _id } = playlist;
+    let cellContent = null;
     function onDeleteButtonClick(event: SyntheticEvent): void {
       event.preventDefault();
-      onPlaylistDelete(playlist);
+      onPlaylistDelete(cell.row.original);
     }
-
-    return (
-      <tr key={_id}>
-        <td>
+    switch (cell.column.id) {
+      case 'title':
+        cellContent =
           <Link
             title={_id}
             to={generatePath(PLAYLIST_SHOW, { _id })}>
-            {title}
+            {cell.value}
           </Link>
-        </td>
-        <td>{formatDate({ date: created })}</td>
-        <td>{formatDate({ date: accessed })}</td>
-        <td><a href="#" onClick={onDeleteButtonClick}>Delete</a></td>
-      </tr>
-    );
+        break;
+      case 'accessed':
+      case 'created':
+        cellContent = formatDate({ date: cell.value });
+        break;
+      case 'delete':
+        cellContent = <a href="#" onClick={onDeleteButtonClick}>Delete</a>;
+        break;
+      default:
+        cellContent = cell.value;
+        break;
+    }
+    return <td {...cell.getCellProps()}>{cellContent}</td>;
   }
 
   return (
 		<div className="all-playlists-view">
       <h1>playlist://<span className="highlight">all</span></h1>
-      <table className="all-playlists-table">
+      <table {...getTableProps()} className="all-playlists-table">
         <thead>
           <tr>
-            <th>Title</th>
-            <th>Created on</th>
-            <th>Last accessed</th>
-            <th></th>
+          {headers.map(column => (
+            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+          ))}
           </tr>
         </thead>
-        <tbody>{playlists.map(renderTableRow)}</tbody>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(
+            row => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(renderCell)}
+                </tr>
+              )}
+          )}
+        </tbody>
       </table>
     </div>
 	);
