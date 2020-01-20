ea02dde31cb9c447b156e5375d5cac972d4facc3
Diego Caponera <diego.caponera@gmail.com>
2020-01-20 19:59:55 +0100

    Refactor contextMenu helper.

--- src/renderer/utils/contextMenuUtils.ts
+++ src/renderer/utils/contextMenuUtils.ts
@@ -1,4 +1,4 @@
-import { remote, ipcRenderer as ipc } from 'electron';
+import { remote, ipcRenderer as ipc, MenuItemConstructorOptions } from 'electron';
 const { Menu, MenuItem } = remote;
 import { playTrack } from '../store/modules/player';
 import { IPC_MESSAGES, SEARCH_URLS } from '../../constants';
@@ -30,37 +30,36 @@ export default function openContextMenu(options: ContextMenuOptions, dispatch?: 
   const menu = new Menu();
   const query = `${options.context.artist} ${options.context.title}`;
   const fullTitle = `${options.context.artist} - ${options.context.title}`;
+  let menuItems: MenuItemConstructorOptions[];
   switch (options.type) {
     case ContextMenuTypes.RESULT_LIST_ITEM:
     case ContextMenuTypes.ALBUM_COVER:
-      menu.append(new MenuItem({
-        label: `Play '${fullTitle}'`,
-        click(): void {
-          dispatch(playTrack({
-            albumId: options.context._id
-          }));
+      menuItems = [
+        {
+          label: `Play '${fullTitle}'`,
+          click(): void { dispatch(playTrack({ albumId: options.context._id })) }
         },
-      }));
-      menu.append(new MenuItem({ type: 'separator' }));
-      menu.append(new MenuItem({
-        label: `Show '${fullTitle}' in Finder`,
-        click(): void {
-          ipc.send(IPC_SYS_REVEAL_IN_FINDER, options.context.path);
+        { type: 'separator' },
+        {
+          label: `Reveal '${fullTitle}' in Finder`,
+          click(): void { ipc.send(IPC_SYS_REVEAL_IN_FINDER, options.context.path) }
         },
-      }));
-      menu.append(new MenuItem({
-        label: `Search '${fullTitle}' on rateyourmusic`,
-        click(): void {
-          ipc.send(IPC_SYS_OPEN_URL, SEARCH_URLS.RYM, query);
+        { type: 'separator' },
+        {
+          label: `Search '${fullTitle}' on rateyourmusic`,
+          click(): void { ipc.send(IPC_SYS_OPEN_URL, SEARCH_URLS.RYM, query) }
         },
-      }));
-      menu.append(new MenuItem({
-        label: `Search '${fullTitle}' on Discogs`,
-        click(): void {
-          ipc.send(IPC_SYS_OPEN_URL, SEARCH_URLS.DISCOGS, query);
+        {
+          label: `Search '${fullTitle}' on rateyourmusic`,
+          click(): void { ipc.send(IPC_SYS_OPEN_URL, SEARCH_URLS.RYM, query) }
         },
-      }));
+        {
+          label: `Search '${fullTitle}' on Discogs`,
+          click(): void { ipc.send(IPC_SYS_OPEN_URL, SEARCH_URLS.DISCOGS, query) }
+        }
+      ];
       break;
   }
+  menuItems.forEach(item => menu.append(new MenuItem(item)));
   menu.popup({ window: remote.getCurrentWindow() });
 }
