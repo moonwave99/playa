b18981dd195111e23b3265370543d89e5d2632ef
Diego Caponera <diego.caponera@gmail.com>
2020-01-30 18:27:11 +0100

    Fix test setup warning.

--- test/setup.ts
+++ test/setup.ts
@@ -1,5 +1,5 @@
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter = require('enzyme-adapter-react-16');
 const globalAny: any = global;
 
 globalAny.AudioContext = jest.fn().mockImplementation(() => {
