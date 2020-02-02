d30cb511388976ed0dccbe4e470f89c4e543042d
Diego Caponera <diego.caponera@gmail.com>
2020-02-02 16:44:45 +0100

    Add tests for NewPlaylistButton.

---
+++ src/renderer/components/SidebarView/NewPlaylistButton/NewPlaylistButton.test.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { renderInDnDProvider, mountInDnDProvider } from '../../../../../test/testUtils';
+
+import { NewPlaylistButton } from './NewPlaylistButton';
+
+describe('NewPlaylistButton tests', () => {
+  it('should render a .new-playlist-button', () => {
+		const wrapper = renderInDnDProvider(
+			<NewPlaylistButton
+        onClick={jest.fn()}
+        onDrop={jest.fn()}
+			/>
+		);
+		expect(wrapper.is('.new-playlist-button')).toBe(true);
+  });
+
+  it('should call the onClick handler when clicked', () => {
+    const handler = jest.fn();
+    const wrapper = mountInDnDProvider(
+      <NewPlaylistButton
+        onClick={handler}
+        onDrop={jest.fn()}
+			/>
+    );
+    wrapper.find('button').simulate('click');
+    expect(handler).toHaveBeenCalled();
+  });
+});
