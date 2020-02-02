f53630923dfc5977cf1ab42171f04462069c275b
Diego Caponera <diego.caponera@gmail.com>
2020-02-02 23:53:31 +0100

    Add test placeholder for PlaylistContainer.

---
+++ src/renderer/components/PlaylistContainer/PlaylistContainer.test.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { renderInAll, mockRouter } from '../../../../test/testUtils';
+import { playlists, albums } from '../../../../test/testFixtures';
+import { toObj } from '../../utils/storeUtils';
+
+mockRouter({
+  routeParams: { _id: '1' },
+  routeMatch: { url: '/playlist/1' }
+});
+
+import { PlaylistContainer } from './PlaylistContainer';
+
+const defaultStore = {
+  player: {
+    queue: [] as string[]
+  },
+  playlists: {
+    allById: toObj(playlists)
+  },
+  albums: {
+    allById: toObj(albums)
+  },
+  tracks: {
+    allById: {}
+  },
+  covers: {
+    allById: {}
+  },
+  waveforms: {
+    allById: {}
+  }
+};
+
+describe('PlaylistContainer tests', () => {
+  it.skip('should contain a .playlist-view', () => {
+    const wrapper = renderInAll(<PlaylistContainer/>, defaultStore);
+    expect(wrapper.find('.playlist-view')).toHaveLength(1);
+  });
+});
