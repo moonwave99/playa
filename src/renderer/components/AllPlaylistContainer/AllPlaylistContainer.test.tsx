5563cf9167862f75e417d33d5c17ffda1e69dbe4
Diego Caponera <diego.caponera@gmail.com>
2020-02-03 13:51:46 +0100

    Add tests for AllPlaylistContainer.

---
+++ src/renderer/components/AllPlaylistContainer/AllPlaylistContainer.test.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { renderInAll } from '../../../../test/testUtils';
+import { playlists, albums } from '../../../../test/testFixtures';
+import { toObj } from '../../utils/storeUtils';
+
+import { AllPlaylistContainer } from './AllPlaylistContainer';
+
+const defaultStore = {
+  player: {
+    queue: [] as string[]
+  },
+  playlists: {
+    allById: toObj(playlists)
+  },
+  albums: {
+    allById: toObj(albums)
+  },
+  tracks: {
+    allById: {}
+  },
+  covers: {
+    allById: {}
+  },
+  waveforms: {
+    allById: {}
+  }
+};
+
+describe('AllPlaylistContainer tests', () => {
+  it('should contain a .all-playlists-view', () => {
+    const wrapper = renderInAll(
+      <AllPlaylistContainer playlists={playlists}/>
+      , defaultStore);
+    expect(wrapper.is('.all-playlists-view')).toBe(true);
+  });
+});
