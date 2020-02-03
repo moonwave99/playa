4bdd604650ccbb236c8cb96b91945ad124a618d8
Diego Caponera <diego.caponera@gmail.com>
2020-02-03 22:22:28 +0100

    Update tests for QueueView.

--- src/renderer/components/QueueView/QueueView.test.tsx
+++ src/renderer/components/QueueView/QueueView.test.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { renderInAll } from '../../../../test/testUtils';
-
+import { albums } from '../../../../test/testFixtures';
+import { toObj } from '../../utils/storeUtils';
 import { QueueView } from './QueueView';
 
 const defaultStore = {
@@ -11,7 +12,7 @@ const defaultStore = {
     allById: {}
   },
   albums: {
-    allById: {}
+    allById: toObj(albums)
   },
   tracks: {
     allById: {}
@@ -38,5 +39,17 @@ describe('QueueView tests', () => {
   it('should render a .queue-empty-placeholder if queue is empty', () => {
     const wrapper = renderInAll(<QueueView/>, defaultStore);
     expect(wrapper.find('.queue-empty-placeholder')).toHaveLength(1);
+    expect(wrapper.find('.album-list')).toHaveLength(0);
+  });
+
+  it('should render an .album-list if queue is not empty', () => {
+    const wrapper = renderInAll(<QueueView/>, {
+      ...defaultStore,
+      player: {
+        queue: albums.map(({ _id }) => _id)
+      }
+    });
+    expect(wrapper.find('.queue-empty-placeholder')).toHaveLength(0);
+    expect(wrapper.find('.album-list')).toHaveLength(1);
   });
 });
