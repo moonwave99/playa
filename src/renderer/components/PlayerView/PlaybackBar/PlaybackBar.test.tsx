7856acfe68ba114614efb7ad37056fa3241690da
Diego Caponera <diego.caponera@gmail.com>
2020-02-03 12:52:50 +0100

    Add tests for PlaybackBar.

---
+++ src/renderer/components/PlayerView/PlaybackBar/PlaybackBar.test.tsx
@@ -0,0 +1,118 @@
+import * as React from 'react';
+import { renderInAll, mountInAll } from '../../../../../test/testUtils';
+import { playlists, albums, tracks } from '../../../../../test/testFixtures';
+import { toObj } from '../../../utils/storeUtils';
+import { formatDuration } from '../../../utils/datetimeUtils';
+
+import { PlaybackBar } from './PlaybackBar';
+
+const defaultStore = {
+  player: {
+    queue: [] as string[]
+  },
+  playlists: {
+    allById: toObj(playlists)
+  },
+  albums: {
+    allById: toObj(albums)
+  },
+  tracks: {
+    allById: toObj(tracks)
+  },
+  covers: {
+    allById: {}
+  },
+  waveforms: {
+    allById: {}
+  }
+};
+
+describe('PlaybackBar tests', () => {
+  it('should render a .playback-bar', () => {
+    const wrapper = renderInAll(
+			<PlaybackBar
+        currentAlbum={albums[0]}
+        currentTrack={tracks[0]}
+        currentTime={0}
+        duration={tracks[0].duration}
+        waveform='/path/to/waveform'
+				onWaveformNotFound={jest.fn()}
+				onProgressBarClick={jest.fn()}/>,
+			defaultStore);
+    expect(wrapper.is('.playback-bar')).toBe(true);
+  });
+
+  it('should contain a .waveform', () => {
+    const wrapper = renderInAll(
+			<PlaybackBar
+        currentAlbum={albums[0]}
+        currentTrack={tracks[0]}
+        currentTime={0}
+        duration={tracks[0].duration}
+        waveform='/path/to/waveform'
+				onWaveformNotFound={jest.fn()}
+				onProgressBarClick={jest.fn()}/>,
+			defaultStore);
+    expect(wrapper.find('.waveform')).toHaveLength(1);
+  });
+
+  it('should display the current track information', () => {
+    const wrapper = renderInAll(
+			<PlaybackBar
+        currentAlbum={albums[0]}
+        currentTrack={tracks[0]}
+        currentTime={0}
+        duration={tracks[0].duration}
+        waveform='/path/to/waveform'
+				onWaveformNotFound={jest.fn()}
+				onProgressBarClick={jest.fn()}/>,
+			defaultStore);
+    expect(wrapper.find('.current-track-title').text()).toBe(tracks[0].title);
+    expect(wrapper.find('.current-track-info').text()).toBe(`${tracks[0].artist} - ${albums[0].title}`);
+  });
+
+  it('should display the elapsed time', () => {
+    const wrapper = renderInAll(
+			<PlaybackBar
+        currentAlbum={albums[0]}
+        currentTrack={tracks[0]}
+        currentTime={0}
+        duration={tracks[0].duration}
+        waveform='/path/to/waveform'
+				onWaveformNotFound={jest.fn()}
+				onProgressBarClick={jest.fn()}/>,
+			defaultStore);
+    expect(wrapper.find('.duration-elapsed').text()).toBe(formatDuration(0));
+  });
+
+  it('should display the time left', () => {
+    const wrapper = renderInAll(
+			<PlaybackBar
+        currentAlbum={albums[0]}
+        currentTrack={tracks[0]}
+        currentTime={0}
+        duration={tracks[0].duration}
+        waveform='/path/to/waveform'
+				onWaveformNotFound={jest.fn()}
+				onProgressBarClick={jest.fn()}/>,
+			defaultStore);
+    expect(wrapper.find('.duration-left').text()).toBe(`-${formatDuration(tracks[0].duration)}`);
+  });
+
+  it('should call the onProgressBarClick handler when progress bar is clicked', () => {
+    const handler = jest.fn();
+    const wrapper = mountInAll(
+			<PlaybackBar
+        currentAlbum={albums[0]}
+        currentTrack={tracks[0]}
+        currentTime={0}
+        duration={tracks[0].duration}
+        waveform='/path/to/waveform'
+				onWaveformNotFound={jest.fn()}
+				onProgressBarClick={handler}/>,
+			defaultStore);
+
+    wrapper.simulate('click');
+    expect(handler).toHaveBeenCalled();
+  });
+});
