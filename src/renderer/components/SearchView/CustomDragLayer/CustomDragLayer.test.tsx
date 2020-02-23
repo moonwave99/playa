31e30fa2853c74cdefa4380c3fd09b5f6eab05dc
Diego Caponera <diego.caponera@gmail.com>
2020-02-24 00:07:29 +0100

    Add tests for CustomDragLayer.

---
+++ src/renderer/components/SearchView/CustomDragLayer/CustomDragLayer.test.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { renderInAll } from '../../../../../test/testUtils';
+
+import { CustomDragLayer } from './CustomDragLayer';
+
+describe('CustomDragLayer', () => {
+  it('should render nothing if user is not dragging', () => {
+    const wrapper = renderInAll(
+      <CustomDragLayer/>
+    );
+    expect(wrapper.find('.custom-drag-layer')).toHaveLength(0);
+  });
+});
