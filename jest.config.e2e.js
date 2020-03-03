cdd5b0b2c465c342d50c206db89081ed2780ae05
Diego Caponera <diego.caponera@gmail.com>
2020-03-04 00:16:52 +0100

    (app) Remove unused jest configuration.

--- jest.config.e2e.js
+++ jest.config.e2e.js
@@ -3,7 +3,7 @@ module.exports = {
     "<rootDir>/test-e2e"
   ],
   testMatch: [
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    "**/?(*.)+(test).+(ts|tsx|js)"
   ],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest"
