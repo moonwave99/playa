b7b4cd31b164444877489aa166a28c9ac7505ee8
Diego Caponera <diego.caponera@gmail.com>
2020-03-09 11:48:48 +0100

    (app) Use floats in Library layout.

--- src/renderer/components/LibraryView/AlbumGridView/AlbumGridView.tsx
+++ src/renderer/components/LibraryView/AlbumGridView/AlbumGridView.tsx
@@ -1,7 +1,6 @@
-import React, { ReactElement, FC } from 'react';
+import React, { FC } from 'react';
 import { AlbumGridTileView } from './AlbumGridTileView/AlbumGridTileView';
 import { Album } from '../../../store/modules/album';
-import { LIBRARY_GRID_COLUMN_COUNT } from '../../../../constants';
 
 import './AlbumGridView.scss';
 
@@ -18,33 +17,15 @@ export const AlbumGridView: FC<AlbumGridViewProps> = ({
   onAlbumContextMenu,
   onAlbumDoubleClick
 }) => {
-
-  function renderAlbum(album: Album): ReactElement {
-    return (
-      <AlbumGridTileView
-        key={album._id}
-        album={album}
-        isPlaying={album._id === currentAlbumId}
-        onDoubleClick={onAlbumDoubleClick}
-        onContextMenu={onAlbumContextMenu}/>
-    );
-  }
-
-  const rows = albums.reduce((memo, item, index) => {
-    const rowIndex = Math.floor(index / LIBRARY_GRID_COLUMN_COUNT);
-    if (!memo[rowIndex]) {
-      memo.push([]);
-    }
-    memo[rowIndex].push(item);
-    return memo;
-  }, []);
-
 	return (
     <section className="album-grid">
-      {rows.map((row, index) =>
-        <div className="album-grid-row" key={index}>
-          {row.map(renderAlbum)}
-        </div>
+      {albums.map(album =>
+        <AlbumGridTileView
+          key={album._id}
+          album={album}
+          isPlaying={album._id === currentAlbumId}
+          onDoubleClick={onAlbumDoubleClick}
+          onContextMenu={onAlbumContextMenu}/>
       )}
     </section>
 	);
