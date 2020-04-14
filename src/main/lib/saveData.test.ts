87d9b420da6a459c973f7d41a7359ee84951b84f
Diego Caponera <diego.caponera@gmail.com>
2020-04-14 17:06:04 +0200

    (app) Update saveData tests.

--- src/main/lib/saveData.test.ts
+++ src/main/lib/saveData.test.ts
@@ -5,5 +5,10 @@ describe('saveData', () => {
     expect(async () => {
       await saveData('data', '/tmp/file', 'utf8')
     }).not.toThrow();
+    try {
+      await saveData('data', '/this/path/does/not/exist', 'utf8')
+    } catch (error) {
+      expect(error.message).toBe('Error writing to /this/path/does/not/exist');
+    }
   });
 })
