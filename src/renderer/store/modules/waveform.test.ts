eb6aa0e936dd0b084c74144317e6868bf62a6966
Diego Caponera <diego.caponera@gmail.com>
2020-04-16 12:28:16 +0200

    (app) Update waveform tests.

--- src/renderer/store/modules/waveform.test.ts
+++ src/renderer/store/modules/waveform.test.ts
@@ -3,19 +3,27 @@ import thunk from 'redux-thunk';
 const mockStore = configureStore([thunk]);
 
 import { tracks } from '../../../../test/testFixtures';
+import { EntityHashMap } from '../../utils/storeUtils';
 import reducer, {
   WaveformActionTypes,
   WaveformState,
   getWaveformRequest,
+  selectors,
   WAVEFORM_GET_REQUEST,
   WAVEFORM_GET_RESPONSE
 } from './waveform';
 
+import { ApplicationState } from '../store';
+
 jest.mock('../../lib/waveform', () => {
   return {
     Waveform: class{
-      getSVGPath(): string { return 'data'; }
-      async load(): Promise<void> { true }
+      path: string;
+      constructor(options: { path: string }) {
+        this.path = options.path;
+      }
+      getSVGPath(): string { return this.path === '' ? null : 'data'; }
+      async load(): Promise<void> { return }
     }
   }
 });
@@ -32,6 +40,15 @@ describe('waveform actions', () => {
       ];
       await getWaveformRequest(tracks[0])(store.dispatch);
       expect(store.getActions()).toEqual(expectedActions);
+    });
+
+    it('should dispatch nothing if waveform content could not be retrieved', async () => {
+      const store = mockStore({});
+      await getWaveformRequest({
+        ...tracks[0],
+        path: ''
+      })(store.dispatch);
+      expect(store.getActions()).toEqual([]);
     });
   });
 });
@@ -61,6 +78,36 @@ describe('waveform reducer', () => {
     })).toEqual({
       ...initialState,
       allById: { [tracks[0]._id]: path }
+    });
+  });
+});
+
+describe('waveform selectors', () => {
+  const state = {
+    waveforms: {
+      allById: {
+        '1': '/path/to/waveform'
+      } as EntityHashMap<string>
+    }
+  } as ApplicationState;
+  describe('state', () => {
+    it('should return the waveforms state', () => {
+      const selection = selectors.state(state);
+      expect(selection).toEqual(state.waveforms);
+    });
+  });
+
+  describe('allById', () => {
+    it('should return waveforms.allById', () => {
+      const selection = selectors.allById(state);
+      expect(selection).toEqual(state.waveforms.allById);
+    });
+  });
+
+  describe('findById', () => {
+    it('should find an waveform by id', () => {
+      const selection = selectors.findById(state, '1');
+      expect(selection).toEqual('/path/to/waveform');
     });
   });
 });
