3e1261830b3e6be716211fc30b30aba7c9519565
Diego Caponera <diego.caponera@gmail.com>
2020-05-28 16:46:52 +0200

    Update website.

--- site/app/initialize.js
+++ site/app/initialize.js
@@ -5,6 +5,19 @@ require('prismjs/components/prism-jsx');
 require('prismjs/components/prism-tsx');
 require('prismjs/components/prism-yaml');
 
+const albums = [
+  'The Smiths - Meat is Murder',
+  'Low - I Could Die in Hope',
+  'Gustavo Cerati - Bocanada',
+  'Dirty Three - Ocean Songs',
+  'Slowdive - Outside Your Room EP',
+  'This Mortal Coil - It\'ll End in Tears',
+  'My Bloody Valentine - You Made Me Realise EP',
+  'Ecstasy Of Saint Theresa - Pigment',
+  'Alice in Chains - Jar of Flies',
+  'Swans - White Light From The Mouth Of Infinity'
+];
+
 function shuffleArray(array) {
   const copy = array.slice(0);
   for (let i = copy.length - 1; i > 0; i--) {
@@ -33,9 +46,14 @@ function fillAlbumBar({
     album.style.top = 0;
     album.style.width = `${albumWidth}px`;
     album.style.transform = `translateX(${(i - offset / 2) * albumWidth}px)`;
+
+    const { src, alt } = randomisedImages[i % images.length];
+
     const img = document.createElement('img');
     img.onload = () => img.classList.add('loaded');
-    img.src = randomisedImages[i % images.length];
+    img.src = src;
+    img.alt = alt;
+    img.title = alt;
     album.appendChild(img);
 
     element.appendChild(album);
@@ -70,7 +88,10 @@ module.exports = function(_options){
       .forEach(element => fillAlbumBar({
         element,
         observer,
-        images: [...Array(10).keys()].map(i => `${options.basePath}/images/albums/${i}.jpg`)
+        images: albums.map((alt, i) => ({
+          src: `${options.basePath}/images/albums/${i}.jpg`,
+          alt
+        }))
       }));
 
     const $screenshots = document.querySelectorAll('.hero-screenshots img');
