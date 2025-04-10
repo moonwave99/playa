af5d52b017ea399f7489d14c0a79935e5b68c5f7
moonwave99 <diego.caponera@gmail.com>
2025-04-10 09:33:38 +0200

    (app) add loading icon

--- src/renderer/components/Loading.tsx
+++ src/renderer/components/Loading.tsx
@@ -1,5 +1,10 @@
 import styles from "./Loading.module.css";
+import LoadingIcon from "../../assets/loading.svg?react";
 
 export default function Loading() {
-    return <div className={styles.loading}>Loading...</div>;
+    return (
+        <div className={styles.Loading}>
+            <LoadingIcon className={styles.LoadingIcon} />
+        </div>
+    );
 }
