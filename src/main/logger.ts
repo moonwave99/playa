7a8e7fabc9fff1728e64a67d47f94dc39135d1d2
moonwave99 <diego.caponera@gmail.com>
2025-04-14 12:07:48 +0200

    (app) finally

--- src/main/logger.ts
+++ src/main/logger.ts
@@ -1,11 +1,11 @@
-import logConfig from '../../logConfig.json';
+import loggerConfig from '../../loggerConfig.json';
 
 export function log(key: string, ...params: unknown[]) {
   if (process.env.TEST) {
     return;
   }
   const tokens = key.split(':');
-  if (!(logConfig as Record<string, boolean>)[tokens[0]]) {
+  if (!(loggerConfig as Record<string, boolean>)[tokens[0]]) {
     return;
   }
   console.log(key, ...params);
