c8167513d518ce1989d8e1f9bc727ccf63444833
moonwave99 <diego.caponera@gmail.com>
2025-10-21 23:57:23 +0200

    Improve containing entities e2e specs

--- e2e-tests/artists/containingGroups.spec.ts
+++ e2e-tests/artists/containingGroups.spec.ts
@@ -7,7 +7,7 @@ test.describe.configure({ mode: "serial" });
 const getElectronApp = setupElectron();
 
 test.describe("Containing Groups", () => {
-  test("add the current Artist to a Group", async () => {
+  test("add the current Artist to an existing Group", async () => {
     const electronApp = getElectronApp();
     const page = await electronApp.firstWindow();
 
@@ -44,6 +44,43 @@ test.describe("Containing Groups", () => {
     ).toBeVisible();
   });
 
+  test("add the current Artist to a new Group", async () => {
+    const electronApp = getElectronApp();
+    const page = await electronApp.firstWindow();
+
+    await page.getByRole("button", { name: "Toggle Menu" }).click();
+    await page.getByLabel("Go to the Artists page").click();
+
+    await expect(
+      page.locator('[data-testid="breadcrumbs"]').getByText("Artists")
+    ).toBeVisible();
+
+    await page.getByRole("button", { name: "Show latest Artists" }).click();
+
+    await page
+      .locator('[data-testid="LatestArtistsView"]')
+      .getByText("Artist 10")
+      .click();
+
+    await expect(page.locator('[data-testid="ArtistPage"]')).toBeVisible();
+
+    await clickMenuItemById(electronApp, "addArtistToGroup");
+    const modal = page.locator(".ReactModalPortal");
+    await expect(modal.getByText("Add Artists to Group").first()).toBeVisible();
+
+    await page.getByLabel("Add to Group").fill("New Group");
+    await page.waitForTimeout(100);
+
+    await page.keyboard.press("Enter");
+    await page.keyboard.press("Enter");
+    await page.keyboard.press("Escape");
+
+    await expect(page.getByText("Appears in")).toBeVisible();
+    await expect(
+      page.locator('[data-testid="ArtistPageHeader"]').getByText("New Group")
+    ).toBeVisible();
+  });
+
   test("removes the current Artist from a Group", async () => {
     const electronApp = getElectronApp();
     const page = await electronApp.firstWindow();
@@ -74,9 +111,19 @@ test.describe("Containing Groups", () => {
       .getByLabel("Remove Artist 10 from Group 1")
       .click();
 
-    await expect(page.getByText("Appears in")).not.toBeVisible();
     await expect(
       page.locator('[data-testid="ArtistPageHeader"]').getByText("Group 1")
     ).not.toBeVisible();
+
+    await page
+      .locator('[data-testid="ArtistPageHeader"]')
+      .getByLabel("Remove Artist 10 from New Group")
+      .click();
+
+    await expect(
+      page.locator('[data-testid="ArtistPageHeader"]').getByText("New Group")
+    ).not.toBeVisible();
+
+    await expect(page.getByText("Appears in")).not.toBeVisible();
   });
 });
--- e2e-tests/releases/containingCollections.spec.ts
+++ e2e-tests/releases/containingCollections.spec.ts
@@ -7,7 +7,7 @@ test.describe.configure({ mode: "serial" });
 const getElectronApp = setupElectron();
 
 test.describe("Containing Collections", () => {
-  test("add the current Artist to a Group", async () => {
+  test("add the current Release to an existing Collection", async () => {
     const electronApp = getElectronApp();
     const page = await electronApp.firstWindow();
 
@@ -43,7 +43,43 @@ test.describe("Containing Collections", () => {
     ).toBeVisible();
   });
 
-  test("removes the current Artist from a Group", async () => {
+  test("add the current Release to a new Collection", async () => {
+    const electronApp = getElectronApp();
+    const page = await electronApp.firstWindow();
+
+    await page.getByRole("button", { name: "Toggle Menu" }).click();
+    await page.getByLabel("Go to the Home page").click();
+
+    await expect(page.locator('[data-testid="HomePage"]')).toBeVisible();
+    await page.waitForTimeout(100);
+
+    await page.keyboard.press("ArrowRight");
+    await page.keyboard.press("Enter");
+
+    await expect(page.locator('[data-testid="ReleasePage"]')).toBeVisible();
+
+    await clickMenuItemById(electronApp, "addReleasesToCollection");
+    const modal = page.locator(".ReactModalPortal");
+    await expect(
+      modal.getByText("Add Releases to Collection").first()
+    ).toBeVisible();
+
+    await page.getByLabel("Add to Collection").fill("New Collection");
+    await page.waitForTimeout(100);
+
+    await page.keyboard.press("Enter");
+    await page.keyboard.press("Enter");
+    await page.keyboard.press("Escape");
+
+    await expect(page.getByText("Appears in")).toBeVisible();
+    await expect(
+      page
+        .locator('[data-testid="ReleaseWithTracklistHeader"]')
+        .getByText("New Collection")
+    ).toBeVisible();
+  });
+
+  test("removes the current Release from a Collection", async () => {
     const electronApp = getElectronApp();
     const page = await electronApp.firstWindow();
 
@@ -68,11 +104,23 @@ test.describe("Containing Collections", () => {
       .getByLabel("Remove Release 2-5 from Collection 1")
       .click();
 
-    await expect(page.getByText("Appears in")).not.toBeVisible();
     await expect(
       page
         .locator('[data-testid="ReleaseWithTracklistHeader"]')
         .getByText("Collection 1")
     ).not.toBeVisible();
+
+    await page
+      .locator('[data-testid="ReleaseWithTracklistHeader"]')
+      .getByLabel("Remove Release 2-5 from New Collection")
+      .click();
+
+    await expect(
+      page
+        .locator('[data-testid="ReleaseWithTracklistHeader"]')
+        .getByText("New Collection")
+    ).not.toBeVisible();
+
+    await expect(page.getByText("Appears in")).not.toBeVisible();
   });
 });
